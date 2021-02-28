9bf961730a902287cc00b6b66ac06817b01dfa89
NobDod <47255585+NobDod@users.noreply.github.com>
2021-02-28 12:11:24 +0200

    УРА УРА запускалка дллков

---
+++ injector/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GDRPC.Inject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "GDRPC Inject";
+            Thread rpc = new Thread(GDRPC.AppRunner.Run);
+            rpc.Start();
+            while (true)
+                Console.ReadKey(true);
+        }
+    }
+}
